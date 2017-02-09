7bfc12b83d16c3044f1b7ded3e436b0f6c9617fa
Hunter Knepshield <theragingfatcat@gmail.com>
2017-02-09 18:52:59 -0500

    Initial Commit

---
+++ RippleEffectSolver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RippleEffectSolver
+//
+//  Created by Hunter Knepshield on 2/9/17.
+//  Copyright © 2017 Hunter Knepshield. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
