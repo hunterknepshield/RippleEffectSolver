f5de8c7ade8f6a92810312e0a9c3f5acb036f723
Hunter Knepshield <theragingfatcat@gmail.com>
2017-03-05 20:33:16 -0500

    Filling out some beginning logic of creator_main.cc.

--- RippleEffectSolver/creator_main.cc
+++ RippleEffectSolver/creator_main.cc
@@ -14,9 +14,43 @@
 #include "typedefs.h"
 #include "validity_checks.h"
 
+// Verbosity settings. 0 = silent, 1 = print message on action, 2 = print
+// message and board on action.
+int VERBOSITY = 0;
+
+void generateRandomPuzzle() {
+	int width, height;
+	std::cout << "Specify the width of the puzzle to be created... ";
+	std::cin >> width;
+	std::cout << "Specify the height of the puzzle to be created... ";
+	std::cin >> height;
+	
+	Board cellValues, roomIds;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			// cellValues[i][j];
+		}
+	}
+}
+
+void augmentExistingPuzzle() {
+	
+}
 
 int main(void) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
-	return 0;
+	std::cout << "Generate random puzzle (g) or augment existing instance (a)? ";
+	char choice = 'a';
+	bool generateRandom;
+	while (true) {
+		// std::cin >> choice;
+		if (choice == 'g' || choice == 'G') {
+			generateRandomPuzzle();
+			return 0;
+		} else if (choice == 'a' || choice == 'A') {
+			augmentExistingPuzzle();
+			return 0;
+		} else {
+			std::cerr << "Invalid choice. Enter 'g' or 'a'... ";
+		}
+	}
 }
