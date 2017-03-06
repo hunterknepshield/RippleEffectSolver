57326d7b4e336c23f7b76b304f07bf5c445c5fac
Hunter Knepshield <theragingfatcat@gmail.com>
2017-03-05 20:01:47 -0500

    Preparing for creator binary.

---
+++ RippleEffectSolver/creator_main.cc
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RippleEffectCreator
+//
+//  Created by Hunter Knepshield on 3/5/17.
+//  Copyright © 2017 Hunter Knepshield. All rights reserved.
+//
+
+#include <iostream>
+
+int main(void) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
